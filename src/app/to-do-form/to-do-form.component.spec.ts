7b439212db4bb7350b244dbb8bc55ffdc0f463f9
Oleg Shchegolev <madindahouse@gmail.com>
2020-12-07 18:28:39 +0300

    update to Angular 11

--- src/app/to-do-form/to-do-form.component.spec.ts
+++ src/app/to-do-form/to-do-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ToDoFormComponent } from './to-do-form.component';
 
@@ -6,7 +6,7 @@ describe('ToDoFormComponent', () => {
   let component: ToDoFormComponent;
   let fixture: ComponentFixture<ToDoFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ToDoFormComponent ]
     })
